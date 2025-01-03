3f5f51895de597d05c4056aed070c8cce183cb60
Jeffrey Varner <jdv27@cornell.edu>
2025-01-03 17:16:23 -0500

    working on SVD lecture
    working on SVD lecture

---
+++ lectures/week-2/L2c/Include.jl
@@ -0,0 +1,19 @@
+# setup paths -
+const _ROOT = pwd();
+const _PATH_TO_SRC = joinpath(_ROOT, "src");
+const _PATH_TO_DATA = joinpath(_ROOT, "data");
+
+# check do we have a Manifest.toml file?
+using Pkg;
+if (isfile(joinpath(_ROOT, "Manifest.toml")) == false) # have manifest file, we are good. Otherwise, we need to instantiate the environment
+    Pkg.activate("."); Pkg.resolve(); Pkg.instantiate(); Pkg.update();
+end
+
+# load external packages
+using JLD2
+using FileIO
+using Statistics
+using LinearAlgebra
+
+# include my codes
+include(joinpath(_PATH_TO_SRC, "Files.jl"));
