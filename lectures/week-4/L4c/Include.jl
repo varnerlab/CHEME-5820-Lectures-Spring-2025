dbcbb025cf7769df87a86054e6e04105a3b73cb4
Jeffrey Varner <jdv27@cornell.edu>
2025-02-11 14:28:01 -0500

    L4c
    L4c

---
+++ lectures/week-4/L4c/Include.jl
@@ -0,0 +1,26 @@
+# setup paths -
+const _ROOT = pwd();
+const _PATH_TO_SRC = joinpath(_ROOT, "src");
+
+# check do we have a Manifest.toml file?
+using Pkg;
+if (isfile(joinpath(_ROOT, "Manifest.toml")) == false) # have manifest file, we are good. Otherwise, we need to instantiate the environment
+    Pkg.activate("."); Pkg.resolve(); Pkg.instantiate(); Pkg.update();
+end
+
+# load external packages -
+using StatsPlots
+using Plots
+using Colors
+using Statistics
+using LinearAlgebra
+using Distances
+using PrettyTables
+using Random
+
+# load my own packages -
+include(joinpath(_PATH_TO_SRC, "Types.jl"));
+include(joinpath(_PATH_TO_SRC, "Factory.jl"));
+include(joinpath(_PATH_TO_SRC, "Data.jl"));
+include(joinpath(_PATH_TO_SRC, "Compute.jl"));
+include(joinpath(_PATH_TO_SRC, "FiniteDifference.jl"));
