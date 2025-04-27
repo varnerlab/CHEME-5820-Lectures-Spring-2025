6fa165a8b42324e0c460bb2902eb2f89da9ec626
Jeffrey Varner <jdv27@cornell.edu>
2025-04-27 13:25:12 -0400

    L15a
    L15a

---
+++ lectures/week-15/L15a/Include.jl
@@ -0,0 +1,13 @@
+# setup paths -
+const _ROOT = @__DIR__
+const _PATH_TO_SRC = joinpath(_ROOT, "src");
+const _PATH_TO_DATA = joinpath(_ROOT, "data");
+
+# load external packages -
+using Pkg
+if (isfile(joinpath(_ROOT, "Manifest.toml")) == false) # have manifest file, we are good. Otherwise, we need to instantiate the environment
+    Pkg.activate("."); Pkg.resolve(); Pkg.instantiate(); Pkg.update();
+end
+
+# using statements -
+using LinearAlgebra
